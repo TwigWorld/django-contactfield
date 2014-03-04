bb1182d4bf86f98a0406dc33a1ffce87df0dc78d
Colin Barnwell <cbarnwell@twig-it.com>
2014-03-04 16:15:06 +0000

    Fixed potential circular import during syncdb

--- contactfield/__init__.py
+++ contactfield/__init__.py
@@ -1,8 +1,4 @@
-from fields import ContactField, ContactFormField
-from forms import ContactFieldFormMixin
-
-try:
+from django.conf import settings
+if 'south' in settings.INSTALLED_APPS:
     from south.modelsinspector import add_introspection_rules
     add_introspection_rules([], ["^contactfield\.fields\.(ContactField)"])
-except ImportError:
-    pass
