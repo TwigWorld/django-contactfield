4129e8d7ac733c81ddfa182b79ed548b4c50b946
Charlie Quinn <charlesquinn1984@googlemail.com>
2014-05-27 16:15:57 +0100

    bumped version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-contactfield',
-    version='0.1.4',
+    version='0.1.5',
     author='Colin Barnwell',
     packages=['contactfield', 'contactfield.templatetags'],
     description='Customisable contact field for Django',
