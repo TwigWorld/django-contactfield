ae6e2f3da5a2ac53ce272f57078e2c90cb7e36fd
Michael Osarodion <93520480+Mosakoh@users.noreply.github.com>
2021-12-10 16:39:51 +0000

    Support for newer versions of django-jsonfield
    Added support for newer versions of django-jsonfield.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='django-contactfield',
-    version='0.1.6',
+    version='0.1.7',
     author='Colin Barnwell',
     packages=['contactfield', 'contactfield.templatetags'],
     description='Customisable contact field for Django',
     long_description=open('README.md').read(),
     install_requires=[
         "Django >= 1.4",
-        "django-jsonfield==0.9.12b1"
+        "django-jsonfield>=0.9.12b1"
     ],
 )
