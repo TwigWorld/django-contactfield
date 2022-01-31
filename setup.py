7ca33aab2a18e43b1db93d19fc4d5ba91089dbbf
Chris Wright <cjwright83y@gmail.com>
2022-01-31 14:01:07 +0000

    Td 7214 django upgrade (#5)
    * update to work with Django 1.11
    
    * ensure the field is set on a model using `to_python`

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='django-contactfield',
-    version='0.1.7',
+    version='1.0.0',
     author='Colin Barnwell',
     packages=['contactfield', 'contactfield.templatetags'],
     description='Customisable contact field for Django',
     long_description=open('README.md').read(),
     install_requires=[
         "Django >= 1.4",
-        "django-jsonfield>=0.9.12b1"
+        "jsonfield==2.1.1"
     ],
 )
