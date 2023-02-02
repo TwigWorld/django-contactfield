73f606f7d6360d01b2c9ac1c976146cca28cee0f
Michael Davis <mdavis@twigeducation.com>
2023-02-02 12:38:16 +0000

    South should not be used anywhere anymore

--- contactfield/__init__.py
+++
@@ -1,4 +0,0 @@
-from django.conf import settings
-if 'south' in settings.INSTALLED_APPS:
-    from south.modelsinspector import add_introspection_rules
-    add_introspection_rules([], ["^contactfield\.fields\.(ContactField)"])
